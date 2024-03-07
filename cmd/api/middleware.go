3c506117bd415a08ff915d59982765014e2b14f1
Chaitanya Gandhi <60167156+chaitanyagandhi@users.noreply.github.com>
2024-03-07 16:45:02 +0530

    Update middleware.go

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://go-movies-alpha.vercel.app")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		if r.Method == "OPTIONS" {
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
